08be0958fb71cbe97e70385cf128a0f9afb2c0db
nicolasStevan <nicolasstevan32@outlook.com>
2023-04-24 23:20:00 -0300

    Primeiros passos no projeto

---
+++ ListadeTarefas.js
@@ -0,0 +1,38 @@
+const criarTarefa = (evento) => {
+    evento.preventDefault(); //definir comportamento padrão do evento, passando como parametro
+   
+    const dataList = document.querySelector('[data-list]'); //selecionando o elemento que vai receber a tarefa	
+    const inputForm = document.querySelector('[data-form-input]');
+    const valueInput = inputForm.value;
+    
+    const taskLi = document.createElement('li'); //criando elemento li
+    taskLi.classList.add('task'); //adicionando classe ao elemento li
+
+    const content = `<p class="content">${valueInput}</p>`
+
+    taskLi.innerHTML = content; //inserindo o conteudo dentro da li
+
+    dataList.appendChild(taskLi); //inserindo a li dentro da ul, AppendChild adiciona um filho ao elemento
+
+    inputForm.value = ""
+
+    
+}
+
+
+const novaTarefa = document.querySelector('[data-form-button]');
+
+
+novaTarefa.addEventListener('click',criarTarefa);
+
+const botaoConclui = () =>{
+
+    const buttonConclui = document.createElement('button');
+
+    buttonConclui.addEventListener('click', () => {
+       
+    })
+
+    buttonConclui.classList.add('check-button');
+
+}
